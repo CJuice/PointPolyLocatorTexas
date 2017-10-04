0ca8eeba9390ab1925d9ab735238729f8734852f
CSchaefe <CSchaefe@PC055317.ms.tnrcc.state.tx.us>
2017-10-04 08:43:24 -0500

    deleted code from Main

--- PointPolyLocator_Test/src/gov/texas/tceq/LocationService/Main.java
+++ PointPolyLocator_Test/src/gov/texas/tceq/LocationService/Main.java
@@ -3,15 +3,7 @@ package gov.texas.tceq.LocationService;
 public class Main {
 
 	public static void main(String[] args) {
-//		http://localhost:8080/TierIIPointPolyLocator/TierIIGISServlet?latitude=29&longitude=-99
-//		String textFilePath = "http://tceq-sap-mwgd01.tceq.texas.gov/arcgis/rest/directories/arcgisoutput/ConradTesting/Version2e_GPServer/_ags_TierIISpatialQueryOutputFile4.txt";
-//		MyFileReader mfr = new MyFileReader();
-//		mfr.readGPOutputFile(textFilePath);
-//		SpatialDataObject sdo2 = new SpatialDataObject();
-//		boolean answer = sdo2.checkForValidSpatialCoordinates("29", "-109");
-//		System.out.println(answer);
-//		String temp = "";
-//		System.out.println(temp.isEmpty());
+
 	}
 
 }
